a70421faee4221a77da65bb924ee3d24238ac595
Omid Shabab <omidshababdev@gmail.com>
2024-03-09 17:32:43 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center p-24">
+      <Button>Click here!</Button>
+    </main>
+  )
+}
